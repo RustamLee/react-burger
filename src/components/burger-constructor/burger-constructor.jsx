c8d9d745efee3ede1ecab95b0648d9a464d824c6
Rustam Sagaddinov <rustiksagad@gmail.com>
2022-10-19 23:03:49 +0300

    завершить верску(кроме модальных окон)

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,72 +1,99 @@
 import React from "react";
 import styles from './burger-constructor.module.css';
-import { ConstructorElement, Button, DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ConstructorElement, Button, DragIcon, CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 export default function BurgerConstructor() {
   const img = 'https://code.s3.yandex.net/react/code/bun-02.png';
 
   return (
     <div className={styles.constructor}>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', marginTop: '100px' }}>
-        <ConstructorElement
-          type="top"
-          isLocked={true}
-          text="Краторная булка N-200i (верх)"
-          price={200}
-          thumbnail={img}
-        />
-         <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-        <div>
-          <DragIcon type="primary" className={styles.drag}/>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', marginTop: '100px', height: '66vh' }}>
+        <div className={styles.bun}>
           <ConstructorElement
+            type="top"
+            isLocked={true}
             text="Краторная булка N-200i (верх)"
-            price={50}
+            price={200}
             thumbnail={img}
           />
         </div>
-        <div>
-          <DragIcon type="primary" className={styles.drag}/>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }} className={styles.main}>
+          <div>
+            <DragIcon type="primary" className={styles.drag} />
+            <ConstructorElement
+              text="Краторная булка N-200i (верх)"
+              price={50}
+              thumbnail={img}
+            />
+          </div>
+          <div>
+            <DragIcon type="primary" className={styles.drag} />
+            <ConstructorElement
+              text="Краторная булка N-200i (верх)"
+              price={50}
+              thumbnail={img}
+            />
+          </div>
+          <div>
+            <DragIcon type="primary" className={styles.drag} />
+            <ConstructorElement
+              text="Краторная булка N-200i (верх)"
+              price={50}
+              thumbnail={img}
+            />
+          </div>
+          <div>
+            <DragIcon type="primary" className={styles.drag} />
+            <ConstructorElement
+              text="Краторная булка N-200i (верх)"
+              price={50}
+              thumbnail={img}
+            />
+          </div>
+          <div>
+            <DragIcon type="primary" className={styles.drag} />
+            <ConstructorElement
+              text="Краторная булка N-200i (верх)"
+              price={50}
+              thumbnail={img}
+            />
+          </div>
+          <div>
+            <DragIcon type="primary" className={styles.drag} />
+            <ConstructorElement
+              text="Краторная булка N-200i (верх)"
+              price={50}
+              thumbnail={img}
+            />
+          </div>
+          <div>
+            <DragIcon type="primary" className={styles.drag} />
+            <ConstructorElement
+              text="Краторная булка N-200i (верх)"
+              price={50}
+              thumbnail={img}
+            />
+          </div>
+        </div>
+        <div className={styles.bun}>
           <ConstructorElement
-            text="Краторная булка N-200i (верх)"
-            price={50}
+            type="bottom"
+            isLocked={true}
+            text="Краторная булка N-200i (низ)"
+            price={200}
             thumbnail={img}
           />
         </div>
-        <div>
-          <DragIcon type="primary" className={styles.drag}/>
-          <ConstructorElement
-            text="Краторная булка N-200i (верх)"
-            price={50}
-            thumbnail={img}
-          />
-        </div>
-        <div>
-          <DragIcon type="primary" className={styles.drag}/>
-          <ConstructorElement
-            text="Краторная булка N-200i (верх)"
-            price={50}
-            thumbnail={img}
-          />
-        </div>
-        <div>
-          <DragIcon type="primary" className={styles.drag}/>
-          <ConstructorElement
-            text="Краторная булка N-200i (верх)"
-            price={50}
-            thumbnail={img}
-          />
-        </div>
-        </div>
-        <ConstructorElement
-          type="bottom"
-          isLocked={true}
-          text="Краторная булка N-200i (низ)"
-          price={200}
-          thumbnail={img}
-        />
       </div>
-      <Button type="primary" size="medium">Оформить заказ</Button>
+      <div className={styles.button}>
+        <div>
+          <div className={styles.total}>
+            <p className="text text_type_digits-medium mr-3">610</p>
+            <CurrencyIcon type='primary' />
+          </div>
+        </div>
+        <Button type="primary" size="medium" >Оформить заказ</Button>
+      </div>
     </div>
   )
-
 }
