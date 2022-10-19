83165393f95969072f46d3b308fa4d22499e5cd9
Rustam Sagaddinov <rustiksagad@gmail.com>
2022-10-19 19:06:08 +0300

    доделать разметку конструктора

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,70 +1,72 @@
 import React from "react";
 import styles from './burger-constructor.module.css';
-import {ConstructorElement, Button, DragIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+import { ConstructorElement, Button, DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
-export default function BurgerConstructor () {
-const img = 'https://code.s3.yandex.net/react/code/bun-02.png';
-    return (
-        <div className = {styles.constructor}>
-            <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-                
-      <ConstructorElement
-        type="top"
-        isLocked={true}
-        text="Краторная булка N-200i (верх)"
-        price={200}
-        thumbnail={img}
-      />
-      <div>
-      <DragIcon />
-      <ConstructorElement
-        text="Краторная булка N-200i (верх)"
-        price={50}
-        thumbnail={img}
-      />
-      </div>
-      <div>
-      <DragIcon />
-      <ConstructorElement
-        text="Краторная булка N-200i (верх)"
-        price={50}
-        thumbnail={img}
-      />
-      </div>
-      <div>
-      <DragIcon />
-      <ConstructorElement
-        text="Краторная булка N-200i (верх)"
-        price={50}
-        thumbnail={img}
-      />
-      </div>
-      <div>
-      <DragIcon />
-      <ConstructorElement
-        text="Краторная булка N-200i (верх)"
-        price={50}
-        thumbnail={img}
-      />
-      </div>
-      <div>
-      <DragIcon />
-      <ConstructorElement
-        text="Краторная булка N-200i (верх)"
-        price={50}
-        thumbnail={img}
-      />
-      </div>
-      <ConstructorElement
-        type="bottom"
-        isLocked={true}
-        text="Краторная булка N-200i (низ)"
-        price={200}
-        thumbnail={img}
-      />
-    </div>
-            <Button type="primary" size="medium">Оформить заказ</Button>
+export default function BurgerConstructor() {
+  const img = 'https://code.s3.yandex.net/react/code/bun-02.png';
+
+  return (
+    <div className={styles.constructor}>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', marginTop: '100px' }}>
+        <ConstructorElement
+          type="top"
+          isLocked={true}
+          text="Краторная булка N-200i (верх)"
+          price={200}
+          thumbnail={img}
+        />
+         <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+        <div>
+          <DragIcon type="primary" className={styles.drag}/>
+          <ConstructorElement
+            text="Краторная булка N-200i (верх)"
+            price={50}
+            thumbnail={img}
+          />
         </div>
-    )
+        <div>
+          <DragIcon type="primary" className={styles.drag}/>
+          <ConstructorElement
+            text="Краторная булка N-200i (верх)"
+            price={50}
+            thumbnail={img}
+          />
+        </div>
+        <div>
+          <DragIcon type="primary" className={styles.drag}/>
+          <ConstructorElement
+            text="Краторная булка N-200i (верх)"
+            price={50}
+            thumbnail={img}
+          />
+        </div>
+        <div>
+          <DragIcon type="primary" className={styles.drag}/>
+          <ConstructorElement
+            text="Краторная булка N-200i (верх)"
+            price={50}
+            thumbnail={img}
+          />
+        </div>
+        <div>
+          <DragIcon type="primary" className={styles.drag}/>
+          <ConstructorElement
+            text="Краторная булка N-200i (верх)"
+            price={50}
+            thumbnail={img}
+          />
+        </div>
+        </div>
+        <ConstructorElement
+          type="bottom"
+          isLocked={true}
+          text="Краторная булка N-200i (низ)"
+          price={200}
+          thumbnail={img}
+        />
+      </div>
+      <Button type="primary" size="medium">Оформить заказ</Button>
+    </div>
+  )
 
 }
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { CurrencyIcon, Counter } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from './burger-ingredients.module.css'
 import Tab from "../tabs/tab";
 
@@ -9,53 +9,59 @@ export default function BurgerIngredients({ ingredients }) {
     <div className={styles.ingredients}>
       <h1 className={`${styles.title} text text_type_main-large`}>Соберите бургер</h1>
       <Tab />
-      <h2 className={styles.block}>Булки</h2>
-      <section className={styles.ingredient}>
-        {ingredients && ingredients.map((element) => {
-          if (element.type === 'bun')
-            return <div key={element._id}>
-              <div className={styles.card}>
-                <img src={element.image} alt={element.name} className = {styles.image}/>
-                <div className={styles.price}><p>{element.price}</p>
-                  <CurrencyIcon type='primary' />
-                </div>
-                <p className= {styles.part}>{element.name}</p>
-              </div>
-            </div>
-        }
-        )}
-      </section>
-        <h2>Соусы</h2>
+      <div className={styles.container}>
+        <h2 className={`${styles.title} text text_type_main-medium mt-10`}>Булки</h2>
         <section className={styles.ingredient}>
-        {ingredients && ingredients.map((element) => {
-          if (element.type === 'sauce')
-            return <div key={element._id}>
-              <div className={styles.card}>
-                <img src={element.image} alt={element.name} className = {styles.image}/>
-                <div className={styles.price}><p>{element.price}</p>
-                  <CurrencyIcon type='primary' />
+          {ingredients && ingredients.map((element) => {
+            if (element.type === 'bun')
+              return <div key={element._id}>
+                <div className={styles.card}>
+                  <div><Counter id={element._id} count={element.count} size="default" /></div>
+                  <img src={element.image} alt={element.name} className={styles.image} />
+                  <div className={styles.price}><p className="text text_type_digits-default mr-2">{element.price}</p>
+                    <CurrencyIcon type='primary' />
+                  </div>
+                  <p className={`${styles.part} text text_type_main-default`}>{element.name}</p>
                 </div>
-                <p className= {styles.part}>{element.name}</p>
               </div>
-            </div>
-        }
-        )}
-      </section>
-      <h2>Начинки</h2>
-      <section className={styles.ingredient}>
-        {ingredients && ingredients.map((element) => {
-          if (element.type === 'main')
-            return <div key={element._id}>
-              <div className={styles.card}>
-                <img src={element.image} alt={element.name} className = {styles.image}/>
-                <div className={styles.price}><p>{element.price}</p>
-                  <CurrencyIcon type='primary' />
+          }
+          )}
+        </section>
+        <h2 className={`${styles.title} text text_type_main-medium mt-10`}>Соусы</h2>
+        <section className={styles.ingredient}>
+          {ingredients && ingredients.map((element) => {
+            if (element.type === 'sauce')
+              return <div key={element._id}>
+                <div className={styles.card}>
+                  <div><Counter id={element._id} count={element.count} size="default" /></div>
+                  <img src={element.image} alt={element.name} className={styles.image} />
+                  <div className={styles.price}><p className="text text_type_digits-default mr-2">{element.price}</p>
+                    <CurrencyIcon type='primary' />
+                  </div>
+                  <p className={`${styles.part} text text_type_main-default`}>{element.name}</p>
                 </div>
-                <p className= {styles.part}>{element.name}</p>
               </div>
-            </div>
-        })}
-      </section>
+          }
+          )}
+        </section>
+        <h2 className={`${styles.title} text text_type_main-medium mt-10`}>Начинки</h2>
+        <section className={styles.ingredient}>
+          {ingredients && ingredients.map((element) => {
+            if (element.type === 'main')
+              return <div key={element._id}>
+                <div className={styles.card}>
+                  <div><Counter id={element._id} count={element.count} size="default" /></div>
+                  <img src={element.image} alt={element.name} className={styles.image} />
+                  <div className={styles.price}><p className="text text_type_digits-default mr-2">{element.price}</p>
+                    <CurrencyIcon type='primary' />
+                  </div>
+                  <p className={`${styles.part} text text_type_main-default`}>{element.name}</p>
+                </div>
+              </div>
+          })}
+        </section>
+      </div>
+
     </div>
   )
 }
