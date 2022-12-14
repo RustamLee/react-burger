94b2733ae9d1c2f2b370aa327949b05c719a668c
Rustam Sagaddinov <rustiksagad@gmail.com>
2022-12-14 13:18:58 +0300

    правка верстки попап с заказом

--- src/components/feed-details/feed-details.tsx
+++ src/components/feed-details/feed-details.tsx
@@ -14,12 +14,12 @@ export default function FeedDetails() {
     const { ingredients } = useSelector(store => store.ingredientsSet)
 
     const filteredElements = order?.ingredients.map((element) => {
-      return ingredients.find(i => i._id === element)
+        return ingredients.find(i => i._id === element)
     }).map((element) => ({ ...element, key: nanoid() }))
 
     const array = filteredElements?.reduce((acc, item) => {
-      const { price } = item
-      return acc + price!
+        const { price } = item
+        return acc + price!
     }, 0)
 
     return (
@@ -28,23 +28,25 @@ export default function FeedDetails() {
                 <p className={`${styles.number} text text_type_digits-default mt-6 mb-8`}># {order?.number}</p>
                 <p className={`${styles.name} text text_type_main-medium mb-3`}>{order?.name}</p>
                 <p className={`${styles.status}  text text_type_main-small mb-8}`}>{order?.status}
-                
-                
                 </p>
                 <p className="text text_type_main-medium mb-4">Состав:</p>
                 <div>
-                    <ul>
-                        <li className={`mt-4`}>
-                            <div>
-                                <>
-                                    {filteredElements?.map((item, index) => {
-                                        if (item && index < 5)
-                                            return <div className={styles.image} key={item.key}><img src={item.image_mobile} alt={item.name} /></div>
-                                    })}
-                                </>
-                                <p className={`text text_type_main-small`}>Ингредиент</p>
+                    <ul className={styles.img}>
+                        <li className={styles.ingredients}>
+                            <div >
+                                {filteredElements?.map((item, index) => {
+                                    if (item && index < 5)
+                                        return <div>
+                                            <div className={styles.image} key={item.key}><img src={item.image_mobile} alt={item.name} /></div>
+                                            <div className={styles.ingredients}>
+                                                <p> ингредиент</p>
+                                                <p> штук</p>
+                                                <p> сумма</p>
+                                            </div>
+                                        </div>
+                                })}
                             </div>
-                            <div>
+                            <div className={styles.summ}>
                                 <span className={`text text_type_digits-default mr-2`}>Цена</span>
                                 <CurrencyIcon type='primary' />
                             </div>
